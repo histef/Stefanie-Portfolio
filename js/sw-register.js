805e7a4233bb9255579ad735331436a561b887fe
Stefanie Chin <hi.stef.c@gmail.com>
2018-11-29 21:28:47 -0800

    add scope

--- js/sw-register.js
+++ js/sw-register.js
@@ -1,7 +1,7 @@
 /******Register Service Worker*****/
 
 if ("serviceWorker" in navigator){
-  navigator.serviceWorker.register('sw.js')
+  navigator.serviceWorker.register('sw.js', {scope: 'https://histef.github.io/Stefanie-Portfolio/'})
   .then(registration => console.log('Service Worker Registered'))
   .catch(err => console.log(`Service Worker registration failed ${err}`));
 }
