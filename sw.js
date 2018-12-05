fa13d4156c1470a57ca00dffaff3c5f23708df34
Stefanie Chin <hi.stef.c@gmail.com>
2018-12-05 14:49:01 -0800

    fine-tune: contrast,skills pop ups

--- sw.js
+++ sw.js
@@ -34,13 +34,23 @@ self.addEventListener('fetch', event => {
   event.respondWith( //tells browser we're handling this request ourselves
     //takes a response object (new Response()) or a promise that resolves with a response
     //gets something out of any cache, starting with the oldest
-      caches.match(event.request).then(function(response) {
-        if (response) {
-         return response  //check cache first, if not there, then fetch from network
-        } return fetch(event.request);
+      caches.match(event.request)
+        .then( res => {
+        if (res) {
+         return res;  //check cache first, if not there, then fetch from network
+        } else {
+          let resClone = res.clone();
+          caches.open(cacheName)
+          .then(cache => {
+            cache.put(event.request, resClone);
+          })
+          return res
+        }
+        return fetch(event.request);
       })
     );
 
+  //  checks if initial request (from server) fails(no connection)
   //  fetch(event.request).then(response=>{
   //    if (response.status === 404){
   //      return new Response("Non-existent page");
@@ -52,10 +62,11 @@ self.addEventListener('fetch', event => {
   // );
 });
 
+
+//clears anything in old cache
 self.addEventListener('activate', event => {
   console.log('[ServiceWorker] activated');
   event.waitUntil(
-  //clear old cache versions
     //get all cache names that exist
   caches.keys().then(cacheNames => Promise.all( //wait til we get everything to delete all at once.
     cacheNames.map(oldCache => {
